a18ae8f5a30509eb8ceb7683ac10827bcf6a220b
Elazar Leibovich <elazarl@gmail.com>
2016-08-15 13:35:33 +0300

    Allocate memory from 100MB permanent address
    This would make tiny guest with <10MB fail, but
    would allow us to allocate memory even for large
    guests.
    
    Remember only the first 1GB or so are allocated with
    phys-to-virt 1 on 1 mapping.
    So previously, we allocated from the largest physical
    address, giving us addresses which are not mapped.

--- memory.cc
+++ memory.cc
@@ -5,14 +5,15 @@
 extern "C" void *_memset(void *p, int val, size_t sz);
 namespace memory {
 u8 *max_page_addr;
+u8 *allocation_addr = reinterpret_cast<u8 *>(1024*1024*10);
 
 extern logger::module memory;
 
 u8 *alloc_page() {
-    logger::debug(memory, "had %x now %x\n", max_page_addr, max_page_addr-4096);
-    max_page_addr -= 4096;
-    memset(max_page_addr, 0, 4096);
-    return max_page_addr;
+    logger::debug(memory, "had %x now %x\n", allocation_addr, allocation_addr-4096);
+    allocation_addr -= 4096;
+    memset(allocation_addr, 0, 4096);
+    return allocation_addr;
 }
 
 void free_page(void *page) {}
